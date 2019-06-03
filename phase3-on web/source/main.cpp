f555e6c305f98bd8ebdd9ce0a97a973734314b4b
AmirHosseinMissMe <50926437+AmirHosseinMissMe@users.noreply.github.com>
2019-06-03 11:11:21 +0430

    Update main.cpp
    change to make one home page

--- phase3-on web/source/main.cpp
+++ phase3-on web/source/main.cpp
@@ -15,10 +15,7 @@ int main(int argc, char **argv) {
     server.post("/", new LoginHandler(&network));
     server.get("/signup", new ShowPage("static/signup.html"));
     server.post("/signup", new SignUpHandler(&network));
-    server.get("/publishershome", new PublishedFilmListHandler(&network));
-    server.get("/publishershomefilter", new PublishedFilterFilmListHandler(&network));
-    server.get("/customershome", new AllowedFilmListHandler(&network));
-    server.get("/customershomefilter", new AllowedFilterFilmListHandler(&network));
+    server.get("/home", new FilmListHandler(&network));
     server.get("/profile", new BoughtFilmListHandler(&network));
     server.get("/detailoffilm", new DetailFilmHandler(&network));
     server.get("/rate", new ScoreFilmHandler(&network));
